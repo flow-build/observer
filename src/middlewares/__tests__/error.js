2460c093de8e6d81d8cee5177778ccc02cca07e6
MSBarbieri <matheussouzabarbieri@gmail.com>
2021-02-21 19:20:14 -0300

    format

--- src/middlewares/__tests__/error.js
+++ src/middlewares/__tests__/error.js
@@ -11,7 +11,7 @@ describe('check default results from error middleware', () => {
   const conatiner = containerResolver(options);
   const errorMiddleware = conatiner.resolve('errorMiddleware');
   const next = async () => { throw new Error('new message'); };
-  it('default values', async done => {
+  it('default values', async (done) => {
     await errorMiddleware(ctx, next);
 
     expect(ctx.status).toBe(500);
@@ -20,7 +20,7 @@ describe('check default results from error middleware', () => {
     await done();
   });
 
-  it('error without body', async done => {
+  it('error without body', async (done) => {
     await errorMiddleware(ctx, async () => { throw new Error(); });
 
     expect(ctx.status).toBe(500);
